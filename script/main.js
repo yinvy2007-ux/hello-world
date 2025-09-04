041ed91e973902d9be903b4734b2b0c7c89ff03a
yinvy2007-ux <yinvy2007@gmail.com>
2025-09-04 14:25:55 +0700

    Nội dung thay đổi

--- script/main.js
+++ script/main.js
@@ -4,4 +4,15 @@ const canvas = document.getElementById("ufoCanvas");
 canvas.width = 900;
 canvas.height = 750;
 
+function resize () {
 
+    const height = window.innerHeight -20;
+
+    const ratio = canvas.width / canvas.height;
+    const width = height * ratio;
+
+    canvas.style.width = width + 'px';
+    canvas.style.height = height + 'px';
+}
+
+window.addEventListener('load',resize,false);
